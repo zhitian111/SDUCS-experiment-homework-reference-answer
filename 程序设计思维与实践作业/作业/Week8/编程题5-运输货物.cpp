a4d0d67b7748288a0466e0ddea948b13821ab1a3
zhitian111 <167742129+zhitian111@users.noreply.github.com>
2025-04-11 22:26:01 +0800

    程序设计W8T5 complete

--- 程序设计思维与实践作业/作业/Week8/编程题5-运输货物.cpp
+++ 程序设计思维与实践作业/作业/Week8/编程题5-运输货物.cpp
@@ -6,20 +6,20 @@
 #include <queue>
 #include <tuple>
 #include <vector>
+#include <unordered_map>
 using namespace std;
 
 // from to weight time
-typedef tuple<size_t, size_t, long long, long long> Edge;
+typedef tuple<size_t, size_t, size_t , size_t> Edge;
 
 auto INF = numeric_limits<size_t>::max();
 
-size_t spfa_with_limit(vector<vector<Edge>>& edges, size_t from, size_t to,
-                       size_t min_weight, size_t nodes_cnt, size_t time_limit)
+size_t spfa_with_limit(vector<vector<Edge>>& edges, size_t from, size_t to, size_t min_weight, size_t nodes_cnt, size_t time_limit)
 {
   vector<size_t> dist(nodes_cnt + 1, INF);
   dist[from] = 0;
   queue<size_t> q;
-  map<size_t, bool> in_queue;
+  unordered_map<size_t, bool> in_queue;
   q.push(from);
   in_queue[from] = true;
   while (!q.empty())
@@ -44,17 +44,15 @@ size_t spfa_with_limit(vector<vector<Edge>>& edges, size_t from, size_t to,
       {
         continue;
       }
-      if (dist[v] > dist[u] + t)
+      if (dist[v] > dist[u] + t && dist[u] + t <= time_limit)
       {
         dist[v] = dist[u] + t;
-        if (!in_queue[v])
+        if (in_queue[v] == false)
         {
           q.push(v);
           in_queue[v] = true;
         }
       }
-      // cout << "u: " << u << " v: " << v << " w: " << w << " t: " << t <<
-      // endl;
     }
   }
   return INF;
@@ -72,8 +70,8 @@ void solve()
     cin >> N >> M >> T;
     vector<vector<Edge>> edges(N + 1);
     auto nodes_cnt = N;
-    auto edges_cnt = M;
     auto time_limit = T;
+    vector<size_t> weights;
     for (size_t i = 0; i < M; i++)
     {
       size_t u, v;
@@ -81,29 +79,31 @@ void solve()
       size_t t;
       cin >> u >> v >> w >> t;
       max_weight = max(max_weight, w);
+      weights.push_back(w);
       edges[u].push_back(Edge(u, v, w, t));
       edges[v].push_back(Edge(v, u, w, t));
     }
     auto from = 1;
     auto to = N;
     size_t left = 0;
-    auto right = max_weight;
+    auto right = weights.size() - 1;
+    sort(weights.begin(), weights.end());
+    vector<size_t> ans_tmp;
     while (left <= right)
     {
-      auto mid = (left + right) / 2;
-      auto dist = spfa_with_limit(edges, from, to, mid, nodes_cnt, time_limit);
+      auto mid = (left + (right - left) / 2);
+      auto dist = spfa_with_limit(edges, from, to, weights[mid], nodes_cnt, time_limit);
       if (dist == INF)
       {
         right = mid - 1;
       }
       else
       {
+        ans_tmp.push_back(weights[mid]);
         left = mid + 1;
       }
-      // cout << "left: " << left << " right: " << right << endl;
-      // cout << "dist: " << dist << endl;
     }
-    ans.push_back(max(left, right));
+    ans.push_back(*max_element(ans_tmp.begin(), ans_tmp.end()));
   }
   for (auto a : ans)
   {
