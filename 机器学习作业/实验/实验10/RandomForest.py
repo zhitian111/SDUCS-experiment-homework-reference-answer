5c04143ff7832664ab2454ed490292ac428468f4
zhitian111 <167742129+zhitian111@users.noreply.github.com>
2025-04-19 16:32:22 +0800

    机器学习实验10 ~100% | 机器学习实验9 ~100% | 计算机网络实验7 ~100% |

---
+++ 机器学习作业/实验/实验10/RandomForest.py
@@ -0,0 +1,56 @@
+﻿import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_blobs
+import seaborn as sns
+from sklearn.tree import DecisionTreeClassifier
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import BaggingClassifier
+from sklearn.ensemble import RandomForestClassifier
+
+sns.set()
+
+X, y = make_blobs(n_samples=300, centers=4, random_state=0, cluster_std=1.0)
+plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap="rainbow")
+tree = DecisionTreeClassifier().fit(X, y)
+plt.show()
+plt.cla()
+
+def visualize_classifier(model, X, y, ax=None, cmap="rainbow"):
+    ax = ax or plt.gca()
+    # 绘制训练点
+    ax.scatter(
+        X[:, 0], X[:, 1], c=y, s=30, cmap=cmap, clim=(y.min(), y.max()), zorder=3
+    )
+    ax.axis("tight")
+    ax.axis("off")
+    xlim = ax.get_xlim()
+    ylim = ax.get_ylim()
+    # 拟合估计器
+    model.fit(X, y)
+    xx, yy = np.meshgrid(np.linspace(*xlim, num=200), np.linspace(*ylim, num=200))
+    Z = model.predict(np.c_[xx.ravel(), yy.ravel()]).reshape(xx.shape)
+    # 创建颜色图
+    n_classes = len(np.unique(y))
+    contours = ax.contourf(
+        xx,
+        yy,
+        Z,
+        alpha=0.3,
+        levels=np.arange(n_classes + 1) - 0.5,
+        cmap=cmap,
+        # clim=(y.min(), y.max()),
+        zorder=1,
+    )
+    ax.set(xlim=xlim, ylim=ylim)
+
+
+visualize_classifier(DecisionTreeClassifier(), X, y)
+tree = DecisionTreeClassifier()
+bag = BaggingClassifier(tree, n_estimators=100, max_samples=0.8, random_state=1)
+bag.fit(X, y)
+visualize_classifier(bag, X, y)
+
+model = RandomForestClassifier(n_estimators=100, random_state=0)
+visualize_classifier(model, X, y)
+plt.show()
