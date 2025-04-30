c01bca60fb7be27d1607887bd9e9f8a0876179cf
zhitian111 <167742129+zhitian111@users.noreply.github.com>
2025-04-30 16:36:13 +0800

    更新目录关系和程序设计ppt格式

---
+++ 程序设计思维与实践作业/作业/Week11/编程题3-矩阵选数.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve()
+{
+    vector<vector<int>> arr;
+    int n;
+    cin >> n;
+    arr.resize(3);
+    arr[0].resize(n);
+    arr[1].resize(n);
+    arr[2].resize(n);
+    for (int i = 0;i < n;i++)
+    {
+        cin >> arr[0][i];
+    }
+    for (int i = 0;i < n;i++)
+    {
+        cin >> arr[1][i];
+    }
+    for (int i = 0;i < n;i++)
+    {
+        cin >> arr[2][i];
+    }
+    
+}
+int main()
+{
+    solve();
+    return 0;
+}
